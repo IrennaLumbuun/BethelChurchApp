f3da9bfdbd25373ae3048729547dc5390c6744d6
Irenna Lumbuun <irennanicole1@gmail.com>
2020-07-23 22:04:50 -0700

    Initial Commit

---
+++ JCR3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JCR3
+//
+//  Created by Irenna Nicole on 23/07/20.
+//  Copyright © 2020 Irenna Lumbuun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JCR3Tests/JCR3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  JCR3Tests.swift
+//  JCR3Tests
+//
+//  Created by Irenna Nicole on 23/07/20.
+//  Copyright © 2020 Irenna Lumbuun. All rights reserved.
+//
+
+import XCTest
+@testable import JCR3
+
+class JCR3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
